d5a2cdee880ad7f20360b4bda5e2d01d348dd486
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-02-11 19:27:10 +0500

    Some Changes to post making

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
 import CollectionsIcon from "@mui/icons-material/Collections";
 import ArticleIcon from "@mui/icons-material/Article";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 const Container=styled.div`
     grid-area: main;
 `
@@ -70,7 +71,149 @@ div{
   }
 }
 `;
+const Article=styled(CommonCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`
+ const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap:nowrap;
+  padding:12px 16px 0;
+  margin-bottom:8px;
+  align-items:center;
+  display:flex;
+  a{
+    margin-right:12px;
+    flex-grow:1;
+    overflow:hidden;
+    display:flex;
+    text-decoration:none;
+    img{
+      width:48px;
+      height:48px;
+    }
+    & > div{
+      display:flex;
+      flex-direction:column;
+      flex-grow:1;
+      flex-basis:0;
+      margin-left:8px;
+      overflow:hidden;
+      span{
+        text-align:left;
+        &:first-child{
+          font-size:14px;
+          font-weight:700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n +1){
+           font-size:12px;
+           color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button{
+    position:absolute;
+    right: 12px;
+    top:0;
+    background:transparent;
+    border:none;
+    outline: none;
+    
+    img{
+      width:20px;
+      height:20px;
+      padding-bottom: 20px;
+      
+    }
+  }
+ `;
+  const Description = styled.div`
+    padding: 0 16px;
+    overflow: hidden;
+    color: rgba(0, 0, 0, 0.9);
+    font-size: 14px;
+    text-align: left;
+  `;
 
+  const SharedImg = styled.div`
+    margin-top: 8px;
+    width: 100%;
+    display: block;
+    position: relative;
+    background-color: #f9fafb;
+    img {
+      object-fit: contain;
+      width: auto;
+      height: 400px;
+    }
+  `;
+
+  const SocialCount = styled.ul`
+    line-height: 1.3;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1px solid #e9e5df;
+    list-style: none;
+    Button {
+      align-items: center;
+      background-color: transparent;
+      border: none;
+    }
+    li {
+      margin-right: 5px;
+      font-size: 12px;
+      button {
+        display: flex;
+      }
+      img {
+        width: 25px;
+      }
+    }
+  `;
+
+  const SocialAction = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin: 0;
+    min-height: 40px;
+    padding: 4px 70px 8px 9px;
+
+    button {
+      display: inline-flex;
+      align-items: center;
+      color: #0a66c2;
+      width: 50px;
+      height: 50px;
+      background-color: transparent;
+      border: none;
+
+      img {
+        width: 50px;
+        height: 50px;
+      }
+      @media (min-width: 768px) {
+        span {
+          margin-left: 8px;
+        }
+        a {
+          margin-left: 8px;
+        }
+      }
+    }
+  `;
+  const Content = styled.div`
+    text-align: center;
+    & > img {
+      width: 30px;
+    }
+  `;
 const Main=(props)=>{
     return (
       <Container>
@@ -89,7 +232,7 @@ const Main=(props)=>{
             <span>Video</span>
           </button>
           <button>
-            <CollectionsIcon className="icons" style={{ color: "#b2b456" }} />
+            <CollectionsIcon className="icons" style={{ color: "#e19a37ec" }} />
             <span>Event</span>
           </button>
           <button>
@@ -98,6 +241,41 @@ const Main=(props)=>{
           </button>
         </div>
         </ShareBox>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <MoreHorizIcon />
+            </button>
+          </SharedActor>
+          <Description>All cold 🥶🥶.</Description>
+          <SharedImg>
+            <a>
+              <img src="/images/shared-img.jpg" alt="" />
+            </a>
+          </SharedImg>
+          <SocialCount>
+            <li>
+              <button>
+                <img src="/images/like.svg" alt="" />
+                <img src="/images/heart.svg" alt="" />
+                <span>78</span>
+              </button>
+            </li>
+            <li>
+              <a>
+                2 comments
+              </a>
+            </li>
+          </SocialCount>
+        </Article>
       </Container>
     );
 }
