6660be308b3084f15a406f62c654a4f884186957
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-02-19 17:55:48 +0500

    add Login functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
-import {auth,provider} from "../firebase"
+import { auth, provider } from "../firebase";
+import { SET_USER } from "./actionType";
 
-export function signInAPI(){
-    return(dispatch)=>{
-        auth.signInWithPopup(provider).then((payload)=>{
-            console.log(payload)
-        }).catch((error)=>alert(error.message))
-    }
+export const setUser = (payload) => ({
+  type: SET_USER,
+  user: payload,
+});
+
+export function signInAPI() {
+  return (dispatch) => {
+    auth
+      .signInWithPopup(provider)
+      .then((payload) => {
+        dispatch(setUser(payload.user));
+      })
+      .catch((error) => alert(error.message));
+  };
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,32 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 import {connect} from 'react-redux'
 import { signInAPI } from '../actions'
+import { Navigate }  from 'react-router'
 function Login(props) {
   return (
     <Container>
+      {props.user && <Navigate to="/home" />}
       <Nav>
-        <a href='/'>
-            <img src="/images/login-logo.svg" alt="logo" />
+        <a href="/">
+          <img src="/images/login-logo.svg" alt="logo" />
         </a>
         <div>
-            <Join>Join now</Join>
-            <SignIn>Sign in</SignIn>
+          <Join>Join now</Join>
+          <SignIn>Sign in</SignIn>
         </div>
       </Nav>
       <Section>
         <Hero>
-            <h1>Welcome to your professional community</h1>
-            <img src="/images/login-hero.svg" alt="" />
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="" />
         </Hero>
         <Form>
-            <Google onClick={()=>props.signIn()}>
-                <img src="/images/google.svg" alt="" />
-                Sign in with Google
-            </Google>
+          <Google onClick={() => props.signIn()}>
+            <img src="/images/google.svg" alt="" />
+            Sign in with Google
+          </Google>
         </Form>
       </Section>
     </Container>
-  )
+  );
 }
 const Container=styled.div`
     padding: 0;
